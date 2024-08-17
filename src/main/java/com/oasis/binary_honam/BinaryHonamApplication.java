1729c4c300265b10a23effb886f612154f41431f
hanwldn76 <hanwldn76@gmail.com>
2024-08-17 20:24:34 +0900

    setting: init

---
+++ src/main/java/com/oasis/binary_honam/BinaryHonamApplication.java
@@ -0,0 +1,13 @@
+package com.oasis.binary_honam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinaryHonamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinaryHonamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oasis/binary_honam/BinaryHonamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oasis.binary_honam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinaryHonamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
